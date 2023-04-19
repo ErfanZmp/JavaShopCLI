f5428776c7970e5aa1e4db720c16029c2dc58aa4
Erfan <erfanzamirpour@gmail.com>
2023-04-19 14:07:46 +0330

    Customer Class

---
+++ src/com/cein/erf/Customer.java
@@ -0,0 +1,60 @@
+package com.cein.erf;
+
+public class Customer {
+
+    private static int ID = 100;
+    private final int customerID;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Customer(String name, String address, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.customerID = ID++;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer ID: %d, Name: %s, Phone: %s, Email: %s", customerID, name, phone, email);
+    }
+}
